9ac0615b970d9884d3d431a3acf15c61b8d154f4
KarmaPriyanshu1995 <karmakoders@gmail.com>
2024-07-22 16:51:23 +0530

    chore:  reanimated setup

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import MainApp from './src/screens/mainApp/MainApp'
 import { PersistGate } from 'redux-persist/integration/react'
 import {persistor,store} from './src/redux/store/store';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 const App = () => {
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <GestureHandlerRootView style={{flex: 1}}>
       <MainApp />
+      </GestureHandlerRootView>
       </PersistGate>
       
     </Provider>
---
+++ src/components/customBottomSheet/BottomSheet.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View } from 'react-native';
+import React, { useCallback } from 'react';
+import BottomSheet, { BottomSheetBackdrop, BottomSheetScrollView } from "@gorhom/bottom-sheet";
+
+const AppBottomSheet = React.forwardRef(({ children}, ref) => {
+
+  const renderBackdrop = useCallback(
+		(props) => (
+			<BottomSheetBackdrop
+				{...props}
+				disappearsOnIndex={-1}
+				appearsOnIndex={0}
+			/>
+		),
+		[]
+	);
+  return (
+    <BottomSheet
+    ref={ref}
+    index={-1}
+    enableDynamicSizing
+    backdropComponent={renderBackdrop}
+    enablePanDownToClose
+  >
+    <BottomSheetScrollView contentContainerStyle={styles.contentContainer}>
+      {children}
+    </BottomSheetScrollView>
+  </BottomSheet>
+ 
+  );
+});
+
+export default AppBottomSheet;
+
+const styles = StyleSheet.create({});
